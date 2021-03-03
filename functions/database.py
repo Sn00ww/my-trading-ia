07b6cd0aa327300249397b150d4e0eb9ca4eb885
Sn0w <clement.ozor@epitech.eu>
2021-03-03 23:31:44 +0100

    [main] => Add init database function

--- functions/database.py
+++ functions/database.py
@@ -2,20 +2,42 @@ import mysql.connector
 from mysql.connector import Error
 import pandas as pd
 
+
+
 """ Connect to database """
-def db_connect(host, database, username, password, port):
+def db_connect(host, username, password, port, database):
     connection = None
 
     try:
         connection = mysql.connector.connect(
             host    = host,
-            db      = database,
             user    = username,
             passwd  = password,
             port    = port
         )
-        print("MySQL Database connection successful")
     except Error as err:
         print(f"Error: '{err}'")
 
-    return connection
+    cursor = connection.cursor()
+    cursor.execute("SHOW DATABASES")
+    for x in cursor:
+        if (x[0] == database):
+            return connection
+    init_database(connection, database)
+
+
+
+""" Initialize database """
+def init_database(connection, database):
+    execute_query(connection, "CREATE DATABASE " + database)
+
+
+
+""" Execute a query """
+def execute_query(connection, query):
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute(query)
+    except Error as err:
+        print(f"Error: '{err}'")
--- trade.py
+++ trade.py
@@ -13,4 +13,4 @@ user_db     = config_data['database']['username']
 password_db = config_data['database']['password']
 port_db     = config_data['database']['port']
 
-db_connect(host_db, database_db, user_db, password_db, port_db)
+db_connect(host_db, user_db, password_db, port_db, database_db)
